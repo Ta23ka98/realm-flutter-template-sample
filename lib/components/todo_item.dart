8d6adc12f47b005966d2ba8d564798382010db34
まさたか <shinonome419@icloud.com>
2023-02-05 16:12:46 +0900

    update: Added priority scheme and features

--- lib/components/todo_item.dart
+++ lib/components/todo_item.dart
@@ -32,7 +32,14 @@ class TodoItem extends StatelessWidget {
                 }
               },
             ),
-            title: Text(item.summary),
+            title: Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(right: 8.0),
+                ),
+                SizedBox(width: 175, child: Text(item.summary)),
+              ],
+            ),
             subtitle: Text(
               isMine ? '(mine) ' : '',
               style: boldTextStyle(),
@@ -89,5 +96,31 @@ class TodoItem extends StatelessWidget {
         }
         break;
     }
+  }
+}
+
+class _PriorityIndicator extends StatelessWidget {
+  final int? priority;
+  const _PriorityIndicator(this.priority, {Key? key}) : super(key: key);
+  Widget getIconForPriority(int? priority) {
+    if (priority == PriorityLevel.low) {
+      return const Icon(Icons.keyboard_arrow_down, color: Colors.blue);
+    } else if (priority == PriorityLevel.medium) {
+      return const Icon(Icons.circle, color: Colors.grey);
+    } else if (priority == PriorityLevel.high) {
+      return const Icon(Icons.keyboard_arrow_up, color: Colors.orange);
+    } else if (priority == PriorityLevel.severe) {
+      return const Icon(
+        Icons.block,
+        color: Colors.red,
+      );
+    } else {
+      return const SizedBox.shrink();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return getIconForPriority(priority);
   }
 }
